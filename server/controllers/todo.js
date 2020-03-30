a73776076d2200871ab8c4b2f8d77326933f2c7e
anandapuja <anandapujawandra@gmail.com>
2020-03-30 13:59:25 +0700

    REST Delete clear

--- server/controllers/todo.js
+++ server/controllers/todo.js
@@ -7,8 +7,7 @@ class TodoController {
             res.status(200);
             res.json({ data });
         }).catch( err => {
-            res.status(404);
-            res.json({ msg: 'Data not found' });
+            res.status(500);
         })
     }
     static postTodoData(req,res){
@@ -18,21 +17,76 @@ class TodoController {
             status: req.body.status,
             due_date: req.body.due_date
         }).then( data => {
-            res.status(210);
-            res.json( data );
+            if( data ){
+                res.status(201);
+                res.json( data );
+            } else {
+                res.status(400);
+                res.json( data );
+            }
         }).catch( err => {
             res.status(500);
             res.json({ msg: 'Internal server error' });
         })
     }
     static getTodoData(req,res){
-        res.send('masuk get todo')
+        Todo.findByPk(Number(req.params.id))
+        .then( data => {
+            if( data ){
+                res.status(200);
+                res.json({ data });
+            } else {
+                res.status(404);
+                res.json({ msg: 'Error not found' });
+            }
+        }).catch( err => {
+            res.status(500);
+            res.json({ msg: 'Internal server error'});
+        })
     }
     static putData(req,res){
-        res.send('masuk put todo')
+        const reqbody = {
+            title: req.body.title,
+            description: req.body.description,
+            status: req.body.status,
+            due_date: req.body.due_date
+        }
+        Todo.findByPk({
+            where: {
+                id: req.params.id
+            }
+        }).then(data => {
+            if( data ){
+                return Todo.update(reqbody, { where: { id: req.params.id }})
+            } else {
+                res.status(404);
+                res.json({ msg: 'Error not found' });
+            }
+        }).then( data => {
+            res.status(200);
+            res.json({ data });
+        }).catch( err => {
+            res.status(500);
+            res.json({ msg: 'Internal server error' });
+        })
     }
     static deleteData(req,res){
-        res.send('masuk delete todo')
+        Todo.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then( data => {
+            if( data === 1 ){
+                res.status(200);
+                res.json({ data });
+            } else {
+                res.status(400);
+                res.json({ msg: 'Error not found' });
+            }
+        }).catch( err => {
+            res.status(500);
+            res.json({ msg: 'Error not found' });
+        })
     }
 }
 
